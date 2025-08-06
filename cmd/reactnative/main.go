4ffad767e854b05f6a0a1e244879a8dc49d8964d
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-06 22:20:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/reactnative/main.go
@@ -0,0 +1,23 @@
+// cmd/reactnative/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"reactnative/internal/reactnative"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := reactnative.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
